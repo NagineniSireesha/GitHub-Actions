72a2a50d8dd541d4240a3dac8d75b6085a85ea39
NagineniSireesha <147475770+NagineniSireesha@users.noreply.github.com>
2025-05-20 22:59:56 +0530

    Add files via upload

---
+++ sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+USER="Sireesha Nagineni"
+
+## shellcheck disable=SC2016
+echo 'Hello: $USER'
---
+++ sample1.sh
@@ -0,0 +1,6 @@
+##!/bin/bash
+# shellcheck disable=SC1090-SC2148
+
+USER="Sireesha Nagineni"
+
+echo 'Hello: $USER'
