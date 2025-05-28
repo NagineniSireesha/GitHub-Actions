a7a5f39266b573931011f362510075a3d580774d
NagineniSireesha <147475770+NagineniSireesha@users.noreply.github.com>
2025-05-28 16:54:49 +0530

    Update leak.py

--- scripts/python/leak.py
+++ scripts/python/leak.py
@@ -1,2 +1,2 @@
-aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+aws_access_key_id = "AKIAZC3K7J8FKE4HF3U9"
+aws_secret_access_key = "vj83hdnFEMI/K7MDENG/bPxRfiCY98761234123"
